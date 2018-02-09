72362aba91ab576cc350f7391452ddc03492b778
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-09 08:48:26 -0500

    Updated from Brython Server: 2/9/2018 8:48:20 AM

--- name.py
+++ name.py
@@ -1,1 +1,14 @@
+#Sam Krimmel
+#2/9/18
+#name.py - displays name in color with background
 
+from ggame import *
+
+name = input('What is your name my friend?')
+color = input('Waht is your favourite colour my lad?')
+black = Color(0x000000,1)
+
+
+text = TextAsset(name,fill=, style='bold 60pt Times')
+
+Sprite(text,(450,250))
