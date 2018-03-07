f86031ca5edbcc8e5f95e610cdf26057c6fb6659
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-07 08:39:32 -0500

    Updated from Brython Server: 3/7/2018 8:39:30 AM

--- olympics.py
+++ olympics.py
@@ -2,6 +2,11 @@
 #3/7/18
 #olympics - displays the olympic rings
 
-rings = olympic
+from ggame import *
 
-show(rings)
+white = Color(0xffffff,1)
+black = Color(0x000000,1)
+yellow = Color(0xffe500,1)
+blue = Color(0x4da9f9,1)
+red = Color(0xff0f0f,1)
+green = Color(0x08d63f,1)
