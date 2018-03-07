21791db1aa2ddedc05d97168b46a85f0caebcd0a
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-07 08:21:19 -0500

    Updated from Brython Server: 3/7/2018 8:21:17 AM

--- yield.py
+++ yield.py
@@ -10,8 +10,8 @@ yellow = Color(0xffe500,1)
 yellowOutline = LineStyle(1,yellow)
 blackOutline = LineStyle(4,black)
 triangle = PolygonAsset([(0,0),(300,0),(150,250)], yellowOutline, yellow)
-triangle = PolygonAsset([(50,50),(250,50),(150,200)], blackOutline, yellow)
+triangle2 = PolygonAsset([(50,50),(250,50),(150,200)], blackOutline, yellow)
 
 Sprite(triangle,(200,150))
-
+Sprite(tirangle2,(200,150))
 App().run()
